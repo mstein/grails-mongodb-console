117365301d8d6cb8f78890fd80f5b2436c5d69e8
Manuarii STEIN <manuarii.stein@gmail.com>
2013-11-13 14:13:15 +0100

    bump to grails 2.2.4 for dev mode a bit of clean up in the config files

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,8 +1,8 @@
 log4j = {
     info 'org.codehaus.groovy.grails',
           'org.springframework',
-          'org.hibernate',
-          'net.sf.ehcache.hibernate',
           'org.grails.plugin',
           'org.grails.plugin.mongodb.console.controllers'
 }
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
